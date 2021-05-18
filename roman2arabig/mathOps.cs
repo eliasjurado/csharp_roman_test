68022aeebf1d4bc33a89f52aa5db13888d3704aa
Elias Jurado <ridimdev@gmail.com>
2021-05-18 12:17:07 -0500

    loading source code

---
+++ roman2arabig/mathOps.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace roman2arabig
+{
+    public class mathOps
+    {
+
+
+        public int char_to_int(char c)
+        {
+            switch (c)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return -1;
+            }
+        }
+
+        public int roman_to_Int(String str1)
+        {
+            if (str1.Length == 0 || str1==null)
+            {
+                throw new ArgumentException("Incorrect format");
+            }
+            if (str1.Length == 1)
+            {
+                str1 = onlyOneChar(str1);
+            }
+            else
+            {
+                str1 = twoOrMoreChar(str1);
+            }
+            if (str1 == null) return -1;
+            int num = char_to_int(str1[0]);
+            int pre, curr;
+
+            for (var i = 1; i < str1.Length; i++)
+            {
+                curr = char_to_int(str1[i]);
+                pre = char_to_int(str1[i-1]);
+                if (curr <= pre)
+                {
+                    num += curr;
+                }
+                else
+                {
+                    num = num - pre * 2 + curr;
+                }
+            }
+            return num;
+        }
+
+        public String onlyOneChar (String inputtxt)
+        {
+            Regex rx = new Regex(@"^[IVXLCDM]+$");
+            if(rx.IsMatch(inputtxt))
+            {
+                return inputtxt;
+            }
+            else
+            {
+                return inputtxt.Substring(0, inputtxt.Length - 1);
+            }
+        }
+
+        public String twoOrMoreChar(String inputtxt)
+        {
+            Regex rx = new Regex(@"^[IVXLCDM]+$");
+            var lastLetter = inputtxt.Substring(inputtxt.Length - 1, 1);
+            var beforeLastLetter = inputtxt.Substring(inputtxt.Length - 2, 1);
+            if (rx.IsMatch(inputtxt))
+            {
+                if ((lastLetter == "V" && beforeLastLetter == "I") || (lastLetter == "X" && beforeLastLetter == "I"))
+                {
+                    return inputtxt;
+                }
+                if ((lastLetter == "L" && beforeLastLetter == "X") || (lastLetter == "C" && beforeLastLetter == "X"))
+                {
+                    return inputtxt;
+                }
+                if ((lastLetter == "D" && beforeLastLetter == "C") || (lastLetter == "M" && beforeLastLetter == "C"))
+                {
+                    return inputtxt;
+                }
+                //--------
+                if ((lastLetter == "I" && beforeLastLetter == "X") || (lastLetter == "I" && beforeLastLetter == "V"))
+                {
+                    return inputtxt;
+                }
+                if ((lastLetter == "X" && beforeLastLetter == "L") || (lastLetter == "X" && beforeLastLetter == "C"))
+                {
+                    return inputtxt;
+                }
+                if ((lastLetter == "C" && beforeLastLetter == "D") || (lastLetter == "C" && beforeLastLetter == "M"))
+                {
+                    return inputtxt;
+                }
+                //--------
+                if ((lastLetter == "I" && beforeLastLetter == "I") || (lastLetter == "X" && beforeLastLetter == "X") || (lastLetter == "C" && beforeLastLetter == "C")
+                     || (lastLetter == "M" && beforeLastLetter == "M"))
+                {
+                    return inputtxt;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect format");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Incorrect format");
+            }
+        }
+
+    }
+}
+
+
+
---
+++ roman2arabigTests/mathOpsTests.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using roman2arabig;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace roman2arabig.Tests
+{
+    [TestClass()]
+    public class mathOpsTests
+    {
+
+        [TestMethod()]
+        public void roman_to_intTest_I()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("I"),1);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_II()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("II"), 2);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_III()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("III"), 3);
+        }
+
+
+        [TestMethod()]
+        public void roman_to_intTest_V()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("V"), 5);
+        }
+
+        [TestMethod()]
+        public void roman_to_intTest_X()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("X"), 10);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_XX()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("XX"), 20);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_XXX()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("XXX"), 30);
+        }
+
+        [TestMethod()]
+        public void roman_to_intTest_L()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("L"), 50);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_C()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("C"), 100);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_CC()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("CC"), 200);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_CCC()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("CCC"), 300);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_D()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("D"), 500);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_M()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("M"), 1000);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_MM()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("MM"), 2000);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_MMM()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("MMM"), 3000);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException),"Incorrect format")]
+        public void IncorrectFormat_Empty()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("");
+        }
+
+        [TestMethod()]
+        public void roman_to_intTest_IV()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("IV"), 4);
+        }
+
+        [TestMethod()]
+        public void roman_to_intTest_IX()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("IX"), 9);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_XL()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("XL"), 40);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_XC()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("XC"), 90);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_CD()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("CD"), 400);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_CM()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("CM"), 900);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_XXI()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("XXI"), 21);
+        }
+        [TestMethod()]
+        public void roman_to_intTest_LVIII()
+        {
+            var x = new roman2arabig.mathOps();
+            Assert.AreEqual(x.roman_to_Int("LVIII"), 58);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Incorrect format")]
+
+        public void IncorrectFormat_VX()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("VX");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Incorrect format")]
+        public void IncorrectFormat_LC()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("LC");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Incorrect format")]
+        public void IncorrectFormat_DM()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("DM");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Incorrect format")]
+        public void IncorrectFormat_LL()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("LL");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Incorrect format")]
+        public void IncorrectFormat_DD()
+        {
+            var x = new roman2arabig.mathOps();
+            x.roman_to_Int("DD");
+        }
+    }
+}
